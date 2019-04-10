45bd9fd6795a8e77c41627faf79803123049015e
Feazan <feazany@gmail.com>
2019-04-10 06:16:55 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+app.get('/', (req, res, next) => {
+	res.send('Hello!');
+});
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'hw7'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM assists', function(error, results, fields) {
+	if (error) throw error;
+	console.log('The solution is: ', results[0].solution);
+});
+
+app.listen(3000, (req, res, next) => {
+	console.log('Server Started...');
+});
