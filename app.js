2ef10b1f2c54b4d334bc7e26988422ed0edd3b0e
Feazan <feazany@gmail.com>
2019-04-10 07:25:46 -0400

    building query

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/hw7', (req, res, next) => {
 
 	console.log('Club: ', club, 'Pos: ', pos);
 
-	var query = 'SELECT * FROM assists WHERE Club = ' + "'" + club + "' AND Pos = " + "'" + pos + "'";
+	var query = 'SELECT * FROM assists WHERE Club = ' + "'" + club + "' AND Pos = " + "'" + pos + "' ORDER BY A";
 	console.log('Built Query: ', query);
 	connection.query(query, function(error, results, fields) {
 		if (error) throw error;
