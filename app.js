ad71a8a5d290b97b47858b3c6bc29af99961c6c1
Feazan <feazany@gmail.com>
2019-04-10 16:42:05 -0400

    Integrating memcache

--- app.js
+++ app.js
@@ -24,47 +24,50 @@ app.get('/hw7', (req, res, next) => {
 	var club = req.query.club;
 	var pos = req.query.pos;
 	if (!club || !pos) res.return({ status: 'error', error: 'No query specified' });
-
+	var key = club + pos;
 	console.log('Club: ', club, 'Pos: ', pos);
 
 	var query = 'SELECT * FROM assists WHERE Club = ' + "'" + club + "' AND Pos = " + "'" + pos + "' ORDER BY A DESC";
 	console.log('Built Query: ', query);
-	memcached.get('foo', function(err, data) {
-		console.log('TESTSTSETSET: ', data);
-	});
+	memcached.get(key, function(err, data) {
+		console.log('Data found in cache: ', data);
+		if (!data) {
+			connection.query(query, function(err, results) {
+				if (err) throw error;
 
-	connection.query(query, function(err, results) {
-		if (err) throw error;
-		// cache the results
-		memcached.set('foo', results, 10, function(err) {
-			if (err) console.log(err);
-		});
+				// Compute average assists
+				var avg = 0;
+				for (var i = 0; i < results.length; i++) {
+					avg += results[i].A;
+				}
+				avg /= results.length;
 
-		// Compute average assists
-		var avg = 0;
-		for (var i = 0; i < results.length; i++) {
-			avg += results[i].A;
+				var tie = 0;
+				if (results[0].A === results[1].A) {
+					if (results[0].GS > results[1].GS) {
+						tie = 0;
+					} else {
+						tie = 1;
+					}
+				}
+
+				var response = {
+					club: club,
+					pos: pos,
+					max_assists: results[tie].A,
+					player: results[0].Player,
+					avg_assists: avg
+				};
+
+				// cache the results
+				memcached.set(key, response, 10, function(err) {
+					if (err) console.log(err);
+				});
+				res.send(response);
+			});
+		} else {
+			res.send(data);
 		}
-		avg /= results.length;
-
-		var tie = 0;
-		if (results[0].A === results[1].A) {
-			if (results[0].GS > results[1].GS) {
-				tie = 0;
-			} else {
-				tie = 1;
-			}
-		}
-
-		var response = {
-			club: club,
-			pos: pos,
-			max_assists: results[tie].A,
-			player: results[0].Player,
-			avg_assists: avg
-		};
-
-		res.send(response);
 	});
 });
 
